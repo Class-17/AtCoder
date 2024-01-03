b4a5e4e59ff7b5333c7a0e864461d53d2e953745
Class-17 <verfolgen17@gmail.com>
2024-01-03 14:47:57 +0800

    01/03/2024

--- Beginner/334/B_Christmas_Trees.cpp
+++ Beginner/334/B_Christmas_Trees.cpp
@@ -10,7 +10,7 @@ i64 f(i64 a, i64 b) {
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-	i64 a, m, l, r;
+    i64 a, m, l, r;
     cin >> a >> m >> l >> r;
     cout << f(r - a, m) - f(l - a - 1, m) << '\n';
     return 0;
