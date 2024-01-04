8977a34d44667d1442f4982898b095bd049033c1
Class-17 <verfolgen17@gmail.com>
2024-01-04 11:46:48 +0800

    review abc_333 a and b

--- Beginner/333/A_Three_Threes.cpp
+++ Beginner/333/A_Three_Threes.cpp
@@ -4,8 +4,6 @@ using namespace std;
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     int n; cin >> n;
-    for (int i = 0; i < n; ++i)
-        cout << n;
-    cout << '\n';
+    cout << string(n, n + '0') << '\n';
     return 0;
 }
--- Beginner/333/B_Pentagon.cpp
+++ Beginner/333/B_Pentagon.cpp
@@ -1,18 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int f(char a, char b) {
-    int x = a - 'A', y = b - 'A';
-    if ((x - 1 + 5) % 5 == y or (x + 1) % 5 == y)
-        return 2;
-    return 1;
+bool near(char a, char b) {
+    if (a > b) swap(a, b);
+    return b - a == 1 or b - a == 4;
 }
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     string s, t;
     cin >> s >> t;
-    if (f(s[0], s[1]) == f(t[0], t[1]))
+    if (near(s[0], s[1]) == near(t[0], t[1]))
         cout << "Yes" << '\n';
     else
         cout << "No" << '\n';
